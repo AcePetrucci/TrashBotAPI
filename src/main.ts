2cea88c982895f4662dfba453c08ee62e14b789a
Andre Petrucci <ace.petrucci@outlook.com>
2020-07-26 11:24:33 -0300

    add port listener

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import * as mongoose from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, '127.0.0.1');
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
